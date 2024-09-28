6b13bc2e2cf31e4a14de861bf889db25900288df
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2024-09-28 20:09:16 +0000

    .

--- PANTY/w1/src/tut/p1.py
+++ PANTY/w1/src/tut/p1.py
@@ -2,6 +2,7 @@
 from playwright.sync_api import sync_playwright, Error
 from datetime import datetime, timezone
 from rich import print as rprint  # For rprinting
+from ..utils.ban import bann2
 import os
 import logging
 from rich.logging import RichHandler
@@ -14,7 +15,7 @@ log = logging.getLogger("rich")
 
 # --- User Agent and Websites ---
 URLS = [
-    "https://nhenta.net/",
+    "https://nhentai.net",
 ]
 uA = "Mozilla/5.0 (Linux; Android 11; Redmi Note 8 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36"
 c_d = datetime.now(timezone.utc).strftime("%Y%m%d_%H%M%S")
@@ -47,7 +48,6 @@ def func1():
             rezStatus = rez.status
             log.info(f"Go to page - {URLS[0]} - {rezStatus}")
         except Exception as e:
-            rezStatus = rez.status
             log.error(f"{e}")
             browser.close()
             return
@@ -58,10 +58,25 @@ def func1():
 
         # ---- All actions Here
         rprint("[orange1][OK] LickingPussy....[/orange1]")
+        # Select the search form
+        search_form = page.query_selector('form[role="search"]')
 
-        # Goto search bar and enter term and click
+        # Enter a term in the search input field
+        search_input = search_form.query_selector('input[name="q"]')
+        search_input.fill("facesitting fart english")
 
-        # ---
+        # Hit enter in the search input field
+        search_input.press("Enter")
+
+        # Wait for the image link to be present in the DOM
+        image_link = page.wait_for_selector(
+            'a.gallerythumb[href="/g/516162/10/"] img.lazyload[data-src="https://t5.nhentai.net/galleries/2962448/10t.png"]',
+            state="attached",
+            timeout=60000,  # Increase the timeout value to 60 seconds
+        )
+
+        # Click the image link
+        image_link.click()
 
         # Create sreenshot - wih current date time
         page.screenshot(path=f"clicks/{c_d}-s1.png", full_page=True)
